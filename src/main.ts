f32b1e593c8a828777964b6af1c55250fe1c0342
Gabriel Fernandes <gfernandescs@gmail.com>
2022-03-31 07:39:15 -0300

    Added helmet and enableCors

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {
@@ -8,6 +9,9 @@ import {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
