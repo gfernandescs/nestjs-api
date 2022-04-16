14fe9b6a5e4998828d86956f85688770e4d1a228
Gabriel Fernandes <gfernandescs@gmail.com>
2022-04-16 17:04:18 -0300

    Refactorings in base.service.ts

--- src/common/bases/base.service.ts
+++ src/common/bases/base.service.ts
@@ -1,14 +1,11 @@
-import { DeepPartial, getRepository, ObjectLiteral } from 'typeorm';
+import { DeepPartial, ObjectLiteral, Repository } from 'typeorm';
 import { EntityFieldsNames } from 'typeorm/common/EntityFieldsNames';
 import { FindConditions } from 'typeorm/find-options/FindConditions';
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
+import { EntityNotExistError } from '../errors/entity-not-exist.error';
 
 interface IListOptions<TEntity> {
-  where?:
-    | FindConditions<TEntity>[]
-    | FindConditions<TEntity>
-    | ObjectLiteral
-    | string
-    | any;
+  where?: FindConditions<TEntity>[] | FindConditions<TEntity> | ObjectLiteral;
   relations?: string[];
   order?: {
     [P in EntityFieldsNames<TEntity>]?: 'ASC' | 'DESC' | 1 | -1;
@@ -19,13 +16,14 @@ interface IListOptions<TEntity> {
 }
 
 export abstract class BaseService<TEntity> {
-  protected constructor(protected readonly entityClass: string) {}
+  protected constructor(
+    protected readonly entityClass: string,
+    protected readonly repository: Repository<TEntity>,
+  ) {}
 
   async create(createDto: DeepPartial<TEntity>) {
-    return getRepository<TEntity>(this.entityClass).save(
-      getRepository<TEntity>(this.entityClass).create(
-        createDto,
-      ) as DeepPartial<TEntity>,
+    return this.repository.save(
+      this.repository.create(createDto) as DeepPartial<TEntity>,
     );
   }
 
@@ -34,34 +32,61 @@ export abstract class BaseService<TEntity> {
   }
 
   async findOne(options: IListOptions<TEntity>): Promise<TEntity> {
-    return getRepository<TEntity>(this.entityClass).findOne(options);
+    return this.repository.findOne(options);
   }
 
   async findById(id: string): Promise<TEntity> {
-    return getRepository<TEntity>(this.entityClass).findOne({
+    const entityExist = await this.repository.findOne({
       where: { id },
     });
+
+    if (!entityExist) {
+      throw new EntityNotExistError(this.entityClass);
+    }
+
+    return entityExist;
   }
 
-  async update(id: string, updateDto: DeepPartial<TEntity>) {
-    return getRepository<TEntity>(this.entityClass).save(
-      getRepository<TEntity>(this.entityClass).create({
-        id,
-        ...updateDto,
-      }) as DeepPartial<TEntity>,
+  async update(
+    criteria: FindConditions<TEntity> | { id: string },
+    updateDto: QueryDeepPartialEntity<TEntity>,
+  ) {
+    const entityExist = await this.repository.findOne({
+      where: criteria,
+    });
+
+    if (!entityExist) {
+      throw new EntityNotExistError(this.entityClass);
+    }
+
+    await this.repository.update(
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      entityExist.id,
+      updateDto,
     );
+
+    return updateDto;
   }
 
-  async remove(id: string) {
-    return getRepository<TEntity>(this.entityClass).delete(id);
+  async remove(criteria: FindConditions<TEntity> | { id: string }) {
+    const entityExist = await this.repository.findOne({
+      where: criteria,
+    });
+
+    if (!entityExist) {
+      throw new EntityNotExistError(this.entityClass);
+    }
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    return this.repository.delete(entityExist.id);
   }
 
   private async findAndCount(options: IListOptions<TEntity>) {
     const { where, pagination, order, relations } = this.getQuery(options);
 
-    const [result, count] = await getRepository<TEntity>(
-      this.entityClass,
-    ).findAndCount({
+    const [result, count] = await this.repository.findAndCount({
       where,
       relations,
       order,
@@ -76,9 +101,11 @@ export abstract class BaseService<TEntity> {
 
   private getQuery(options: IListOptions<TEntity>) {
     const { order, relations, where, pagination } = options;
+    const defaultQueryLimitNumber =
+      parseInt(process.env.DEFAULT_QUERY_LIMIT_NUMBER) ?? 100;
 
-    if (pagination.take >= 100) {
-      pagination.take = 100;
+    if (pagination.take >= defaultQueryLimitNumber) {
+      pagination.take = defaultQueryLimitNumber;
     }
 
     return { where, pagination, order, relations };
