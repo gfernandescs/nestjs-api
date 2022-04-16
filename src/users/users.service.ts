12e8519715b450dab0327fb848a840e242e17f8c
Gabriel Fernandes <gfernandescs@gmail.com>
2022-04-16 17:04:41 -0300

    Refactorings in users module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { BaseService } from '../common/bases/base.service';
 import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService extends BaseService<User> {
-  constructor() {
-    super(User.name);
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {
+    super(User.name, usersRepository);
   }
 }
